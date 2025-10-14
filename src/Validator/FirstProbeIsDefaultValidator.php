6d3c78d25972867ceff75713df75dc79505e64ad
Arpad Belovai <5740939+belovai@users.noreply.github.com>
2025-10-14 15:31:58 +0200

    Enforce strict types in validators by adding `declare(strict_types=1)`

--- src/Validator/FirstProbeIsDefaultValidator.php
+++ src/Validator/FirstProbeIsDefaultValidator.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Validator;
 
 use App\Repository\ProbeRepository;
@@ -9,7 +11,6 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
 final class FirstProbeIsDefaultValidator extends ConstraintValidator
 {
-
     public function __construct(
         private readonly ProbeRepository $probeRepository,
     ) {
